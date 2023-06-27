5d0acce246b2eeb0996356a993c9398824879f5c
Joyce Lopes Krüger <homeofficina@gmail.com>
2023-06-27 16:10:56 -0300

    exercicios de typescript

--- src/tipando/frota.ts
+++ src/tipando/frota.ts
@@ -1,4 +1,18 @@
-export const frota: {}[] = [
+// A função **buscarCarrosPorMarca** recebe um array de carros e uma marca. Ela devolve os carros desta marca ou o array inteiro, caso a marca não seja passada.
+
+// 1) Faça corretamente a tipagem do arquivo **frota.ts**, que contém os dados dos carros (marca, modelo, ano)
+
+// 2) Importe os dados desse arquivo para o index.ts e faça a tipagem correta da função
+
+// 3) Torne o parâmetro marca opcional
+
+export type TCarro = {
+  marca: string
+  modelo: string
+  ano: Number
+}
+
+export const frota: TCarro [] = [
   {
     marca: 'Chevrolet',
     modelo: 'Onix',
